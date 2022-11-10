df21b997ebc40c6b83b62e72a0a71a4c0440183d
AndersonVT <andersonventosilla0@gmail.com>
2022-11-10 09:17:23 -0500

    midellwar about headers added

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const routes = require("./routes/index");
 dotenv.config();
 app.use(cors());
 app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/api", routes);
 
 app.listen(process.env.PORT, () => {
